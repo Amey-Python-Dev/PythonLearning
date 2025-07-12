e84a4cbabe4299bf7432a9726cefcb172d756be5
Amey Pawar <pawaramey0918@gmail.com>
2025-07-12 15:44:10 +0530

    Solving tasks from Chatgpt journey has been started

---
+++ TasksByChatgpt/01_Task.py
@@ -0,0 +1,9 @@
+#============================================= 01-String Analyzer ========================================================
+
+sentence = input("Enter Your Word/Sentence here: ")
+
+print("Total characters in your Word/Sentence:", len(sentence))
+print("Upper case version of your Word/Sentence:", sentence.upper())
+print("First Character:", sentence[0])
+print("Last character:", sentence[-1])
+print("Reversed word/Sentence:", sentence[::-1])

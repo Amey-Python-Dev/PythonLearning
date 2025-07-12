be7c4a9e3b0f121ef7e07de4cb1de15555eff207
Amey Pawar <pawaramey0918@gmail.com>
2025-07-12 16:18:33 +0530

    Added calculator with nested if and input error handling

--- Projects/01_BasicCalculator.py
+++ Projects/01_BasicCalculator.py
@@ -1,30 +1,33 @@
 #=============================== Calculator ===============================================
 
 print("Welcome to Basic Calculator, by Amey...")
-num1 = float(input("Enter your First number: "))
-num2 = float(input("Enter your Second number: "))
 
-print("Choose operation: +, -, *, /")
-print("For First number to the Power of Second number use: **")
-print("If you use '%' then your numbers will be divided and remainder will be the output")
-opt = input("Enter operator: ")
+try:
+    num1 = float(input("Enter your First number: "))
+    num2 = float(input("Enter your Second number: "))
 
-if (opt == "+"):
-    print("Result:", num1 + num2)
-elif (opt == "-"):
-    print("Result:", num1 - num2)
-elif (opt == "*"):
-    print("Result:", num1 * num2)
-elif (opt == "/"):
-    if (num2 != 0):
-        print("Result:", num1 / num2)
+    print("Choose operation: +, -, *, /, **, %")
+    opt = input("Enter operator: ")
+
+    if opt == "+":
+        print("Result:", num1 + num2)
+    elif opt == "-":
+        print("Result:", num1 - num2)
+    elif opt == "*":
+        print("Result:", num1 * num2)
+    elif opt == "/":
+        if num2 != 0:
+            print("Result:", num1 / num2)
+        else:
+            print("Error: Division by Zero is undefined.")
+    elif opt == "**":
+        print("Result:", num1 ** num2)
+    elif opt == "%":
+        print("Result:", num1 % num2)
     else:
-        print("Error: Division by Zero is undefined.")
-elif (opt == "**"):
-    print("Result:", num1 ** num2)
-elif (opt == "%"):
-    print("Result:", num1 % num2)
-else:
-    print("Invalid Operator!")
-    print("Retry")
-print("Congratulations Your calculation is done!!")
+        print("Invalid Operator! Retry")
+
+    print("Congratulations! Your calculation is done!!")
+
+except ValueError:
+    print("Error: You have entered non-numeric input (like A-Z or special characters). Please try again.")
